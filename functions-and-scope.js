96bfed38446f405cdebe7e44c79f3f3f4fb1f3a3
ShakeelaLab <shakeela@live.nl>
2025-10-18 20:57:44 +0200

    deels gemaakt

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -1,8 +1,24 @@
-// Je gaat functies schrijven die we kunnen hergebruiken om een lijst met eindcijfers van studenten te checken. Je zult over de cijfers heen moeten itereren (hoe pak je dat aan?),
-// maar ook een manier moeten vinden om hetgeen dat je verzamelt ergens te bundelen. Op deze manier zul je ontdekken hoe je omgaat met scope. Pak vooral het hoofdstuk op EdHub over for-loops er nog eens bij!
+// Je gaat functies schrijven die we kunnen hergebruiken om een lijst met eindcijfers van studenten
+// te checken. Je zult over de cijfers heen moeten itereren (hoe pak je dat aan?),
+// maar ook een manier moeten vinden om hetgeen dat je verzamelt ergens te bundelen.
+// Op deze manier zul je ontdekken hoe je omgaat met scope. Pak vooral het hoofdstuk op
+// EdHub over for-loops er nog eens bij!
 // Tip: je mag hier geen ingebouwde object methoden gebruiken, dus daar hoef je niet naar te kijken.
 
+function verzamelCijfers(grades) {
+    const list = [];
+
+    for (let i = 0; i < grades.length; i++) {
+        list[list.length] = grades[i];
+    }
+
+    return list;
+}
+
 const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
+const resultaat = verzamelCijfers(grades);
+console.log(resultaat);
+
 
 /* Opdracht  1: Cum Laude */
 
--- functions-methods.js
+++ functions-methods.js
@@ -1,19 +1,33 @@
-// Je gaat functies schrijven die we kunnen hergebruiken om sommige emailadressen te checken. Nu zul je gaan merken hoe handig functies kunnen zijn!
-// Je zult hier methoden van het String Object voor nodig hebben, dus pak de paragraaf op EdHub over het String Object er even bij.
+// Je gaat functies schrijven die we kunnen hergebruiken om sommige emailadressen te checken.
+// Nu zul je gaan merken hoe handig functies kunnen zijn!
+// Je zult hier methoden van het String Object voor nodig hebben,
+// dus pak de paragraaf op EdHub over het String Object er even bij.
 
 
 /* Opdracht  1 */
-// Schrijf een functie genaamd getEmailDomain, die een emailadres verwacht en de domeinnaam teruggeeft. Een domeinnaam is hetgeen dat na het @ in het adres staat
+// Schrijf een functie genaamd getEmailDomain, die een emailadres verwacht en de domeinnaam teruggeeft.
+// Een domeinnaam is hetgeen dat na het @ in het adres staat
 // ---- Verwachte uitkomsten:
 // getEmailDomain("n.eeken@novi-education.nl") geeft novi-education.nl
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
-// getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
+// // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
+//
 
+function getEmailDomain(emailaddress) {
+    const searchAtDomain = emailaddress.indexOf("@");
+    const domainAddress = emailaddress.substring(searchAtDomain + 1);
+    return domainAddress;
+}
 
-
+const domainOne = getEmailDomain("n.eeken@novi-education.nl");
+const domainTwo = getEmailDomain("t.mellink@novi.nl");
+const domainThree = getEmailDomain("a.wiersma@outlook.com");
+console.log(domainOne,domainTwo,domainThree);
 
 /* Opdracht  2 */
-// Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
+// Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht.
+// De functie checkt of het emailadres een novi domein heeft (medewerker),
+// een novi-education domein (student), of extern domein (zoals gmail of outlook)
 // ---- Verwachte uitkomsten:
 // typeOfEmail("n.eeken@novi-education.nl") geeft "Student"
 // typeOfEmail("t.mellink@novi.nl") geeft geeft "Medewerker"
@@ -21,9 +35,33 @@
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
 
+function typeOfEmail(emailDomain) {
+    const searchAtDomain = emailDomain.indexOf("@");
+    const domainAddress = emailDomain.substring(searchAtDomain + 1);
+    {
+        if (domainAddress === "novi-education.nl") {
+            const novi = "Student";
+            return (novi);
+        } else if (domainAddress === "novi.nl") {
+            const novi = "Medewerker";
+            return (novi);
+        } else {
+            return ("Extern");
+        }
+    }
+}
+
+const emailOne = typeOfEmail("n.eeken@novi-education.nl");
+const emailTwo = typeOfEmail("t.mellink@novi.nl");
+const emailThree = typeOfEmail("novi.nlaapjesk@outlook.com");
+const emailFour = typeOfEmail("a.wiersma@outlook.com");
+
+console.log(emailOne, emailTwo, emailThree, emailFour);
 
 /* Opdracht  3 */
-// Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
+// Schrijf een functie genaamd checkEmailValidity, die een emailadres
+// verwacht en checkt of het emailadres valide is.
+// De functie returned true of false, afhankelijk van de uitkomst.
 // Een emailadres is valide wanneer:
 // * Er een @ in voorkomt
 // * Er géén , in voorkomt
@@ -34,3 +72,19 @@
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
+function checkEmailValidity(valide) {
+    const conditie1 = (valide.includes("@"));
+    const noKomma = !valide.includes(",");
+    const lastPoint = !valide.endsWith(".");
+    const positieApenstaart = valide.indexOf("@");
+    const positieLaatstePunt = valide.lastIndexOf(".");
+    const conditie2 = positieLaatstePunt > positieApenstaart;
+    return conditie1 && conditie2 && noKomma && lastPoint;
+}
+
+const eeken = checkEmailValidity("n.eeken@novi.nl");
+const tess = checkEmailValidity("tessmellink@novi.nl");
+const eeken2 = checkEmailValidity("n.eekenanovi.nl");
+const eeken3 = checkEmailValidity("n.eeken@novinl.");
+console.log(eeken, tess, eeken2, eeken3);
